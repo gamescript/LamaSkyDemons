1deb3cc8909af79c8903812c664a07ab81a0fe28
Frode Lindeijer <frode@lindeijer.nl>
2015-08-14 04:21:19 +0200

    Big cleanup

---
+++ mastercontrol.cpp
@@ -0,0 +1,198 @@
+// LucKey Productions Urho3D Project Template
+//
+// This is free and unencumbered software released into the public domain.
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+//
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// For more information, please refer to <http://unlicense.org/>
+
+#include "mastercontrol.h"
+#include "templatecam.h"
+#include "inputmaster.h"
+
+DEFINE_APPLICATION_MAIN(MasterControl);
+
+MasterControl::MasterControl(Context *context):
+    Application(context),
+    paused_(false)
+{
+}
+
+
+void MasterControl::Setup()
+{
+    engineParameters_["WindowTitle"] = "LucKey Productions Urho3D Project Template";
+    engineParameters_["LogName"] = GetSubsystem<FileSystem>()->GetAppPreferencesDir("urho3d", "logs")+"TestVoxelWidget.log";
+    /*engineParameters_["FullScreen"] = true;
+    engineParameters_["Headless"] = false;
+    engineParameters_["WindowWidth"] = 1980;
+    engineParameters_["WindowHeight"] = 1080;*/
+}
+void MasterControl::Start()
+{
+    new InputMaster(context_, this);
+    cache_ = GetSubsystem<ResourceCache>();
+    graphics_ = GetSubsystem<Graphics>();
+    renderer_ = GetSubsystem<Renderer>();
+
+    defaultStyle_ = cache_->GetResource<XMLFile>("UI/DefaultStyle.xml");
+    CreateConsoleAndDebugHud();
+    CreateScene();
+    CreateUI();
+    SubscribeToEvents();
+
+    /*
+    Sound* music = cache_->GetResource<Sound>("Resources/Music/Macroform_-_Root.ogg");
+    music->SetLooped(true);
+    Node* musicNode = world.scene->CreateChild("Music");
+    SoundSource* musicSource = musicNode->CreateComponent<SoundSource>();
+    musicSource->SetSoundType(SOUND_MUSIC);
+    musicSource->Play(music);
+    */
+}
+void MasterControl::Stop()
+{
+    engine_->DumpResources(true);
+}
+
+void MasterControl::SubscribeToEvents()
+{
+    SubscribeToEvent(E_UPDATE, HANDLER(MasterControl, HandleUpdate));
+    SubscribeToEvent(E_SCENEUPDATE, HANDLER(MasterControl, HandleSceneUpdate));
+}
+
+void MasterControl::CreateConsoleAndDebugHud()
+{
+    Console* console = engine_->CreateConsole();
+    console->SetDefaultStyle(defaultStyle_);
+    console->GetBackground()->SetOpacity(0.8f);
+
+    DebugHud* debugHud = engine_->CreateDebugHud();
+    debugHud->SetDefaultStyle(defaultStyle_);
+}
+
+void MasterControl::CreateUI()
+{
+    cache_ = GetSubsystem<ResourceCache>();
+    UI* ui = GetSubsystem<UI>();
+
+    world.cursor.uiCursor = new Cursor(context_);
+    world.cursor.uiCursor->SetVisible(false);
+    ui->SetCursor(world.cursor.uiCursor);
+    world.cursor.uiCursor->SetPosition(graphics_->GetWidth()/2, graphics_->GetHeight()/2);
+}
+
+void MasterControl::CreateScene()
+{
+    world.scene = new Scene(context_);
+
+    world.scene->CreateComponent<Octree>();
+    world.scene->CreateComponent<DebugRenderer>();
+
+    PhysicsWorld* physicsWorld = world.scene->CreateComponent<PhysicsWorld>();
+//    physicsWorld->SetGravity(Vector3::ZERO);
+
+    //Create a directional light to the world. Enable cascaded shadows on it
+    Node* lightNode = world.scene->CreateChild("DirectionalLight");
+    lightNode->SetPosition(Vector3(-5.0f, 10.0f, -7.0f));
+    lightNode->LookAt(Vector3(0.0f, 0.0f, 0.0f));
+    Light* light = lightNode->CreateComponent<Light>();
+    light->SetLightType(LIGHT_DIRECTIONAL);
+    light->SetBrightness(0.9f);
+    light->SetColor(Color(0.8f, 0.95f, 0.9f));
+    light->SetCastShadows(true);
+    light->SetShadowBias(BiasParameters(0.00025f, 0.5f));
+    light->SetShadowCascade(CascadeParameters(7.0f, 23.0f, 42.0f, 500.0f, 0.8f));
+
+    //Create some Kekelplithfs
+    for (int k = 0; k < 5; k++){
+        Node* objectNode = world.scene->CreateChild("Kekelplithf");
+        objectNode->SetPosition((-5.0f + 3.0f*k) * Vector3::RIGHT + 3.0f * Vector3::DOWN + (k%2) * 5.0f * Vector3::FORWARD);
+        objectNode->SetRotation(Quaternion(0.0f, Random(360.0f), 0.0f));
+        objectNode->SetScale(0.23f+Random(0.0f, 0.1f));
+
+        StaticModel* animatedModel = objectNode->CreateComponent<StaticModel>();
+        animatedModel->SetModel(cache_->GetResource<Model>("Resources/Models/Kekelplithf.mdl"));
+        animatedModel->SetMaterial(cache_->GetResource<Material>("Resources/Materials/Kekelplithf.xml"));
+        animatedModel->SetCastShadows(true);
+    }
+
+    //Create camera
+    world.camera = new TemplateCam(context_, this);
+}
+
+void MasterControl::HandleUpdate(StringHash eventType, VariantMap &eventData)
+{
+}
+
+void MasterControl::HandleSceneUpdate(StringHash eventType, VariantMap &eventData)
+{
+    using namespace SceneUpdate;
+    float timeStep = eventData[P_TIMESTEP].GetFloat();
+}
+
+void MasterControl::HandlePostRenderUpdate(StringHash eventType, VariantMap &eventData)
+{
+}
+
+void MasterControl::Exit()
+{
+    engine_->Exit();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mastercontrol.h
@@ -0,0 +1,148 @@
+// LucKey Productions Urho3D Project Template
+//
+// This is free and unencumbered software released into the public domain.
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+//
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// For more information, please refer to <http://unlicense.org/>
+
+#ifndef MASTERCONTROL_H
+#define MASTERCONTROL_H
+
+#include <Urho3D/Urho3D.h>
+#include <Urho3D/Audio/Sound.h>
+#include <Urho3D/Audio/SoundSource.h>
+#include <Urho3D/Container/HashMap.h>
+#include <Urho3D/Core/CoreEvents.h>
+#include <Urho3D/DebugNew.h>
+#include <Urho3D/Engine/Application.h>
+#include <Urho3D/Engine/Console.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Graphics/AnimatedModel.h>
+#include <Urho3D/Graphics/AnimationController.h>
+#include <Urho3D/Graphics/Camera.h>
+#include <Urho3D/Graphics/DebugRenderer.h>
+#include <Urho3D/Graphics/Graphics.h>
+#include <Urho3D/Graphics/Light.h>
+#include <Urho3D/Graphics/Material.h>
+#include <Urho3D/Graphics/Model.h>
+#include <Urho3D/Graphics/Octree.h>
+#include <Urho3D/Graphics/OctreeQuery.h>
+#include <Urho3D/Graphics/Renderer.h>
+#include <Urho3D/Graphics/RenderPath.h>
+#include <Urho3D/Graphics/StaticModel.h>
+#include <Urho3D/Graphics/Viewport.h>
+#include <Urho3D/Input/Input.h>
+#include <Urho3D/IO/FileSystem.h>
+#include <Urho3D/IO/Log.h>
+#include <Urho3D/Math/MathDefs.h>
+#include <Urho3D/Physics/CollisionShape.h>
+#include <Urho3D/Physics/PhysicsWorld.h>
+#include <Urho3D/Physics/RigidBody.h>
+#include <Urho3D/Resource/ResourceCache.h>
+#include <Urho3D/Resource/Resource.h>
+#include <Urho3D/Resource/XMLFile.h>
+#include <Urho3D/Scene/Node.h>
+#include <Urho3D/Scene/SceneEvents.h>
+#include <Urho3D/Scene/Scene.h>
+#include <Urho3D/UI/Font.h>
+#include <Urho3D/UI/Text.h>
+#include <Urho3D/UI/UI.h>
+#include <Urho3D/Engine/DebugHud.h>
+
+namespace Urho3D {
+class Drawable;
+class Node;
+class Scene;
+class Sprite;
+}
+
+using namespace Urho3D;
+
+class TemplateCam;
+class InputMaster;
+
+typedef struct GameWorld
+{
+    SharedPtr<TemplateCam> camera;
+    SharedPtr<Scene> scene;
+    SharedPtr<Node> voidNode;
+    struct {
+        SharedPtr<Node> sceneCursor;
+        SharedPtr<Cursor> uiCursor;
+        PODVector<RayQueryResult> hitResults;
+    } cursor;
+} GameWorld;
+
+typedef struct HitInfo
+{
+    Vector3 position_;
+    Vector3 hitNormal_;
+    Node* hitNode_;
+    Drawable* drawable_;
+} HitInfo;
+
+namespace {
+StringHash const N_VOID = StringHash("Void");
+StringHash const N_CURSOR = StringHash("Cursor");
+}
+
+class MasterControl : public Application
+{
+    OBJECT(MasterControl);
+    friend class InputMaster;
+public:
+    MasterControl(Context* context);
+    GameWorld world;
+    SharedPtr<ResourceCache> cache_;
+    SharedPtr<Graphics> graphics_;
+
+    /// Setup before engine initialization. Modifies the engine paramaters.
+    virtual void Setup();
+    /// Setup after engine initialization.
+    virtual void Start();
+    /// Cleanup after the main loop. Called by Application.
+    virtual void Stop();
+
+    void Exit();
+private:
+    SharedPtr<UI> ui_;
+    SharedPtr<Renderer> renderer_;
+    SharedPtr<XMLFile> defaultStyle_;
+
+    void CreateConsoleAndDebugHud();
+    void CreateScene();
+    void CreateUI();
+    void SubscribeToEvents();
+
+    void HandleSceneUpdate(StringHash eventType, VariantMap& eventData);
+    void HandleUpdate(StringHash eventType, VariantMap& eventData);
+    void HandlePostRenderUpdate(StringHash eventType, VariantMap& eventData);
+
+    void CreatePlatform(const Vector3 pos);
+    void UpdateCursor(float timeStep);
+    bool CursorRayCast(float maxDistance, PODVector<RayQueryResult> &hitResults);
+
+    bool paused_;
+};
+
+#endif // MASTERCONTROL_H
